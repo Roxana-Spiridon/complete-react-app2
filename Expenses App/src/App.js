2b603f79911f06bdb0b0ee1daccc7deb0ac2f7b0
Rox.Spr <roxana.f.spiridon@gmail.com>
2021-05-21 14:21:37 +0300

    Rendering Lists & Conditional Content

--- Expenses App/src/App.js
+++ Expenses App/src/App.js
@@ -1,39 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Shoes",
+    amount: 120.55,
+    date: new Date(2020, 2, 5),
+  },
+  {
+    id: "e2",
+    title: "Groceries",
+    amount: 67.55,
+    date: new Date(2021, 1, 17),
+  },
+  {
+    id: "e3",
+    title: "Books",
+    amount: 40.6,
+    date: new Date(2021, 2, 20),
+  },
+  {
+    id: "e4",
+    title: "Coffee",
+    amount: 30.25,
+    date: new Date(2021, 3, 23),
+  },
+];
+
 const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Shoes",
-      amount: 120.55,
-      date: new Date(2020, 2, 5),
-    },
-    {
-      id: "e2",
-      title: "Groceries",
-      amount: 67.55,
-      date: new Date(2021, 1, 17),
-    },
-    {
-      id: "e3",
-      title: "Books",
-      amount: 40.6,
-      date: new Date(2021, 2, 20),
-    },
-    {
-      id: "e4",
-      title: "Coffee",
-      amount: 30.25,
-      date: new Date(2021, 3, 23),
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    console.log("In App.js");
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...expenses];
+    });
   };
 
   return (
